2b0e83b6bb745ecf222048a7bfee6edd18548151
Sullivan Muse <sullivan.muse@gmail.com>
2023-09-06 06:42:42 -0500

    Add harpoon

--- init.lua
+++ init.lua
@@ -210,6 +210,11 @@ require('lazy').setup({
 
   'ntpeters/vim-better-whitespace',
 
+  'nvim-lua/plenary.nvim',
+
+  -- Github: https://github.com/ThePrimeagen/harpoon
+  'ThePrimeagen/harpoon',
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
